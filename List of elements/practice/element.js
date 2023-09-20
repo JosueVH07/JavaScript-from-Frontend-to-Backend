85d875532ec20d550b850e51d802c89286eca37a
Josue Velazquez <josuevelazquez0704@gmail.com>
2023-09-20 12:43:48 -0600

    ♻️ Refactor(handle list actions)

--- List of elements/practice/element.js
+++ List of elements/practice/element.js
@@ -1,16 +1,17 @@
 const Element = {
     data() {
         return {
-
+            input: false
         }
     },
     template: `
      <li>
-     <span>{{text}}</span>
+     <span v-if='!input'>{{text}}</span>
+     <input v-else type='text' :value='text' @blur='modify($event)' ref='refInput' />
      &nbsp;&nbsp;&nbsp;
         <button @click="remove()">Remove</button>
         &nbsp;&nbsp;&nbsp;
-        <button @click="modify()">Modify</button>
+        <button @click="input=true">Modify</button>
 
      </li>
     <br />
@@ -18,10 +19,18 @@ const Element = {
     props: ['text', 'index'],
     methods: {
         remove() {
-            this.$emit('remove', { i: this.index});
+            this.$emit('remove', { i: this.index });
+        },
+        modify($event) {
+            var value = event.target.value;
+            this.input = false;
+            this.$emit('modify', { i: this.index, value: value });
         }
     },
-    emits: ['remove']
+    emits: ['remove', 'modify'],
+    updated() {
+        if (this.$refs.refInput) this.$refs.refInput.focus();
+    }
 }
 
 export default Element;
